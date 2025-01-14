9bd2039c85a647d898098b0adcf31056b1f41af0
cek4434 <cemal.ekici.eren@gmail.com>
2025-01-14 14:44:06 +0300

    task service finished

---
+++ src/main/java/com/finalproject/services/TaskService.java
@@ -0,0 +1,57 @@
+package com.finalproject.services;
+
+import com.finalproject.dtos.PermanentTaskDTO;
+import com.finalproject.dtos.TemporaryTaskDTO;
+import com.finalproject.entities.Employee;
+import com.finalproject.entities.Task;
+import com.finalproject.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+
+    private final TaskRepository taskRepository;
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository, EmployeeService employeeService) {
+        this.taskRepository = taskRepository;
+        this.employeeService = employeeService;
+    }
+
+    public Task addTemporaryTask(TemporaryTaskDTO temporaryTaskDTO) {
+
+        Employee isEmployeIdExisist = employeeService.findByEmployeId(temporaryTaskDTO.getCalisanId());
+
+        Task task = temporaryTaskDTO.toEntity();
+        task.setEmployee(isEmployeIdExisist);
+        return taskRepository.save(task);
+
+
+    }
+
+    public Task addPermanentTask(PermanentTaskDTO permanentTaskDTO) {
+
+        Employee isEmployeIdExisist = employeeService.findByEmployeId(permanentTaskDTO.getCalisanId());
+        Task task = permanentTaskDTO.toEntity();
+        task.setEmployee(isEmployeIdExisist);
+
+        return taskRepository.save(task);
+    }
+
+    public List getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public List getTasksByEmployeeId(Long id) {
+        Employee isEmployeIdExisist = employeeService.findByEmployeId(id);
+        return taskRepository.findByEmployeeId(isEmployeIdExisist.getId());
+    }
+
+
+
+}
